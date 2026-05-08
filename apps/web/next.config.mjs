0a2c0f444faf36362756c393f019745cee8405d4
londonbarrett <alondono.dev@gmail.com>
2026-05-08 06:48:44 +0200

    chore: installs charting libraries

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  experimental: {
+    serverComponentsExternalPackages: ["ccxt"],
+  },
 }
 
 export default nextConfig
